6d4c35fa075e8cd8eac0a545bd543ca09490e020
--global <--global>
2026-04-09 14:27:22 -0300

    fix(hero): remove import desnecessário.

--- src/components/hero/HeroModal.tsx
+++ src/components/hero/HeroModal.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
     RiCloseLine,
-    RiCarLine,
     RiSparklingLine,
     RiShieldCheckLine,
     RiBankCardLine,
